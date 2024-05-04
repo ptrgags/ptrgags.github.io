3d679532a3dd913064bb325d10039c20e9e4dc2e
Peter Gagliardi <ptrgags@gmail.com>
2024-05-04 11:31:14 -0400

    Build for 2.1.1

--- assets/LibraryView-9v6aAmCg.js
+++
@@ -1,1 +0,0 @@
-import{p as e,o,a as r}from"./index-8JSi0EGB.js";const s={};function c(t,n){return o(),r("h1",null,"Lots of books here I guess?")}const _=e(s,[["render",c]]);export{_ as default};
---
+++ assets/LibraryView-yzGqOrM5.js
@@ -0,0 +1,1 @@
+import{p as e,o,a as r}from"./index-5GIvy_jU.js";const s={};function c(t,n){return o(),r("h1",null,"Lots of books here I guess?")}const _=e(s,[["render",c]]);export{_ as default};
