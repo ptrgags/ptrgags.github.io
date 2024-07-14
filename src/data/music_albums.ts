916c343a837121f109fc6ffc4a21ff9af40f2241
Peter Gagliardi <ptrgags@gmail.com>
2024-07-14 09:13:28 -0400

    Add track listing for Improvised, Volume 3

--- src/data/music_albums.ts
+++ src/data/music_albums.ts
@@ -3,7 +3,14 @@ import { boo } from './music_albums/boo'
 import { loops } from './music_albums/loops'
 import { improvised_vol2 } from './music_albums/improvised_vol2'
 import { improvised_vol1 } from './music_albums/improvised_vol1'
+import { improvised_vol3 } from './music_albums/improvised_vol3'
 
-const ALBUM_DESCRIPTORS: MusicAlbumDescriptor[] = [boo, improvised_vol1, improvised_vol2, loops]
+const ALBUM_DESCRIPTORS: MusicAlbumDescriptor[] = [
+  boo,
+  improvised_vol1,
+  improvised_vol2,
+  loops,
+  improvised_vol3,
+]
 
 export const ALBUMS = ALBUM_DESCRIPTORS.map((x) => new MusicAlbum(x))
---
+++ src/data/music_albums/improvised_vol3.ts
@@ -0,0 +1,115 @@
+import type { MusicAlbumDescriptor } from '@/core/MusicAlbum'
+
+export const improvised_vol3: MusicAlbumDescriptor = {
+  id: 'improvised-vol3',
+  title: 'Improvised Volume 3',
+  release_date: '2024-07-15',
+  sort_key: '2024-07-25:01',
+  description: `
+        <p>
+            I keep a digital recorder near my synth keyboards so I can record
+            my synth jams. I wait until I fill up the SD card and then
+            edit them in bulk. These improvisations were recorded this spring, 
+            but edited much later.
+        </p>
+        <p>
+            The first half of the album is a collection of miscellaneous
+            synthesizer tracks. The second half is a number of electric piano
+            tracks using my Yamaha Reface CP which emulates classic EPs like
+            Rhodes and Wurlitzers.
+        </p>
+    `,
+  tracks: [
+    {
+      filename: '01_2024-03-07_InclementWeather.flac',
+      title: 'Inclement Weather',
+      date: '2024-03-07',
+      description: `<i>The wind was howling outside, but inside it was nice and comfy.</i>`,
+    },
+    {
+      filename: '02_2024-03-09_DesolateSpaceport.flac',
+      title: 'Desolate Spaceport',
+      date: '2024-03-09',
+      description: `<i>You arrived to the asteroid spaceport 5 minutes too late. Next launch: two years from now...</i>`,
+    },
+    {
+      filename: '03_2024-03-09_HiddenCove.flac',
+      title: 'Hidden Cove',
+      date: '2024-03-09',
+      description: `<i>A hideout away from the waves</i>`,
+    },
+    {
+      filename: '04_2024-03-24_PleasantWhine.flac',
+      title: 'Pleasant Whine',
+      date: '2024-03-24',
+      description: `<i>It wasn't the words but the way it was said that had a nice ebb and flow.</i>`,
+    },
+    {
+      filename: '05_2024-03-24_SquareBells.flac',
+      title: 'Square Bells',
+      date: '2024-03-24',
+      description: `<i>They sound nice and are stackable too!</i>`,
+    },
+    {
+      filename: '06_2024-03-09_SleepInertia.flac',
+      title: 'Sleep Inertia',
+      date: '2024-03-09',
+      description: `<i>That awful feeling when you oversleep and wake up feeling disoriented</i>`,
+    },
+    {
+      filename: '07_2024-03-07_Punchy.flac',
+      title: 'Punchy',
+      date: '2024-03-07',
+      description: `<i>8-bit training montage anyone?</i>`,
+    },
+    {
+      filename: '08_2024-05-02_TimeMachine.flac',
+      title: 'Time Machine',
+      date: '2024-05-02',
+      description: `<i>Whether it goes forwards or backwards in time, no one knows...</i>`,
+    },
+    {
+      filename: '09_2024-05-07_SleepyEP.flac',
+      title: 'Sleepy EP',
+      date: '2024-05-07',
+      description: `<i>yawn</i>`,
+    },
+    {
+      filename: '10_2024-05-07_DeepEP.flac',
+      title: 'Deep EP',
+      date: '2024-05-07',
+      description: `<i>The piano was in the basement for so long it forgot how to play high notes</i>`,
+    },
+    {
+      filename: '11_2024-05-07_WobblyEP.flac',
+      title: 'Wobbly EP',
+      date: '2024-05-07',
+      description: `<i>The keys were just a blur</i>`,
+    },
+    {
+      filename: '12_2024-05-07_AnotherEP.flac',
+      title: 'Another EP',
+      date: '2024-05-07',
+      description: `<i>One more couldn't hurt</i>`,
+    },
+    {
+      filename: '13_2024-03-24_EchoEchoEcho.flac',
+      title: 'Echo Echo Echo',
+      date: '2024-05-07',
+      description: `<i>Enter the echoic chamber</i>`,
+    },
+    {
+      filename: '14_2024-03-24_Spontaneity.flac',
+      title: 'Spontaneity',
+      date: '2024-05-07',
+      description: `<i>Sometimes you need to have fun to change things up.</i>`,
+    },
+    {
+      filename: '15_2024-03-24_ElectricRaindrops.flac',
+      title: 'Electric Raindrops',
+      date: '2024-03-24',
+      description: `<i>pitter patter zap</i>`,
+    },
+  ],
+  play_style: 'advance',
+}
