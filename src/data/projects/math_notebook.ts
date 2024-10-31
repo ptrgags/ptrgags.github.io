2dbbfa3815662c08eb834c8d97a7f595fe58dd39
Peter Gagliardi <ptrgags@gmail.com>
2024-10-31 16:32:49 -0400

    Add a brief project description

--- src/data/projects/math_notebook.ts
+++ src/data/projects/math_notebook.ts
@@ -3,11 +3,15 @@ import type { ProjectDescriptor } from '@/core/Project'
 export const math_notebook: ProjectDescriptor = {
   id: 'math-notebook',
   title: 'Math Notebook',
-  years: '2024',
+  years: '2024-10-31',
   sort_key: '2024-10-30:01',
   github_repo: 'math-notebook',
   img_format: 'png',
   description: `
-    <p>x</p>
+    <p>
+        A Rust project for me to explore mathematical ideas with code.
+        More on this project at a later date. For now, enjoy some
+        spooky math art for Halloween! 🎃👻
+    </p>
   `,
 }
