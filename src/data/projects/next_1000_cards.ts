68c08771ef20a97492558470dd2f011805fa9632
Peter Gagliardi <ptrgags@gmail.com>
2024-11-26 11:30:19 -0500

    Copyediting

--- src/data/projects/next_1000_cards.ts
+++ src/data/projects/next_1000_cards.ts
@@ -6,32 +6,52 @@ export const next_1000_cards: ProjectDescriptor = {
   years: '2024',
   sort_key: '2024-11-11:01',
   description: `
-    <p>An ongoing project to draw 1000 trading card sized doodles.</p>
+    <p>A long-term project to draw 1000 more trading-card sized doodles.</p>
     <p>
         In recent months, I've revisited doodling with fountain pen on paper.
-        Back around college I was doing that a lot, see for example my
-        <a href="https://www.deviantart.com/ptrgags/gallery">DeviantArt gallery</a>.
+        Back around college I was doing that a lot, see for example my old 
+        <a href="https://www.deviantart.com/ptrgags/gallery">DeviantArt gallery</a>
+        from college.
     </p>
     <p>
         One of the key things that kept me drawing often was my art trading
         card binder. I would make trading-card sized artworks (2.5x3.5 inches)
         and store them in a binder with trading card sleeves. It was exciting
-        to see the binder fill up over time. Somewhere around 2021 or 2022
-        I reached the big milestone of drawing 1000 cards for my binder! 
+        to see the binder fill up over time. Somewhere in the lockdown years I
+        reached the big milestone of drawing 1000 cards for my binder!
     </p>
     <p>
-        After that, I put drawing on the shelf for a long while. But recently,
-        when I found myself using fountain pens again for journaling, I decided
-        to doodle again, more passively this time. Most days, I start a
-        journaling session by making a doodle.
+        After that, I put drawing on the shelf for a long while. However,
+        a couple months ago I found myself using fountain pens again for
+        journaling. I decied to doodle again, but more passively this time.
+        I start most journaling sessions by making a doodle.
     </p>
     <p>
-        To give myself something to aim for, I plan to fill a new binder with 
-        1000 cards again, even if that takes years. I'm not posting every 
-        single card. That would be far too tedious to edit, and some cards
-        turn out nicer than others. Instead, I'm occasionally posting my
+        A doodle a day quickly adds up to a large collection. It would
+        be tedious to post every single card, and the quality of cards varies
+        from day to day. Instead, I'll periodically post batches of my
         favorites.
     </p>
   `,
   img_format: 'jpg',
+  updates: [
+    {
+      title: 'Accidental Inktober',
+      date: '2024-10-31',
+      description: `
+        <p>
+            While documenting this first batch of cards, I realized that I drew
+            a lot of cards in october! On 10-01 I drew card #11. On 10-31 I drew
+            card #42. That's 32 cards in total, a hair over 1 card per day!
+        <p>
+        <p>
+            Considering that I've done the <a href="https://inktober.com/">Inktober drawing challenge</a>
+            in past years (see my sketches from <a href="https://www.deviantart.com/ptrgags/gallery/64663431/inktober-2017">2017</a>, <a href="https://www.deviantart.com/ptrgags/gallery/67366134/inktober-2018">2018</a>),
+            I find it amusing that I did the equivalent of an Inktober without
+            intending to this year!
+        </p>
+      `,
+      sort_key: '2024-10-31:01',
+    },
+  ],
 }
