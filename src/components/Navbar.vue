c683ffd7cfa2dbc215ce63f6d48ab3ccaacd8293
Peter Gagliardi <ptrgags@gmail.com>
2026-01-03 08:51:57 -0500

    Add a link to my Bluesky

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,74 +3,81 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <header>
-        <div class="title">
-            <RouterLink to="/">
-                <div class="align-logo">
-                <img class="logo" src="@/assets/monogram.png" alt="PG monogram logo"/>Peter Gagliardi
-                </div>
-            </RouterLink>
+  <header>
+    <div class="title">
+      <RouterLink to="/">
+        <div class="align-logo">
+          <img class="logo" src="@/assets/monogram.png" alt="PG monogram logo" />Peter Gagliardi
         </div>
-        <nav>
-            <ul>
-                <li>
-                    <RouterLink to="/gallery">Gallery</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/projects">Projects</RouterLink>
-                </li>
-                <li>
-                    <a href="https://github.com/ptrgags"><img width="32px" src="@/assets/github-mark-white.svg" /></a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+      </RouterLink>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/gallery">Gallery</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/projects">Projects</RouterLink>
+        </li>
+        <li>
+          <a href="https://github.com/ptrgags"
+            ><img width="32px" src="@/assets/github-mark-white.svg"
+          /></a>
+        </li>
+        <li>
+          <a href="https://bsky.app/profile/ptrgags.bsky.social"
+            ><img width="32px" src="@/assets/bluesky-logo-white.svg"
+          /></a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
 header {
-    height: 60px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    background-color: var(--background-dark);
-    color: var(--color-text);
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: var(--background-dark);
+  color: var(--color-text);
 
-    border-bottom: 10px solid var(--background-transparent);
-    background-clip: padding-box;
+  border-bottom: 10px solid var(--background-transparent);
+  background-clip: padding-box;
 }
 
 .title {
-    justify-self: flex-start;
-    padding: 4px;
-    flex-grow: 1;
+  justify-self: flex-start;
+  padding: 4px;
+  flex-grow: 1;
 }
 
 ul {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 li {
-    /** Turns off the bullets */
-    list-style: none;
-    /** Make them appear on one line */
-    display: inline-block;
-    padding: 4px;
+  /** Turns off the bullets */
+  list-style: none;
+  /** Make them appear on one line */
+  display: inline-block;
+  padding: 4px;
 }
 
 .align-logo {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 .logo {
-    margin: 0px 8px;
+  margin: 0px 8px;
 }
 
 img {
-    display: inline-block;
+  display: inline-block;
 }
 </style>
