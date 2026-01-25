bac6cd74fb6c22bfd89dafde851f4d63f4522ca2
Peter Gagliardi <ptrgags@gmail.com>
2026-01-25 17:06:28 -0500

    Add a function to generate Next 1000 Cards posts

--- src/data/artworks/next_1000_cards.ts
+++ src/data/artworks/next_1000_cards.ts
@@ -1,32 +1,31 @@
 import type { ArtworkDescriptor } from '@/core/Artwork'
 
-export const next_1000_cards_artworks: ArtworkDescriptor[] = [
-  {
-    id: '2024-09-23_Card0003',
-    title: 'Card 3/1000',
-    date: '2024-09-23',
+// Exported only for unit testing
+export function make_atc(iso_date: string, card_id: number): ArtworkDescriptor {
+  const padded_id = card_id.toString().padStart(4, '0')
+  return {
+    id: `${iso_date}_Card${padded_id}`,
+    title: `Card ${card_id}/1000`,
+    date: iso_date,
     timeline_desc: 'Yet another doodle.',
-    sort_key: '2024-09-23:01',
+    sort_key: `${iso_date}:01`,
     project_id: 'next-1000-cards',
     img_format: 'jpg',
-  },
+  }
+}
+
+export const next_1000_cards_artworks: ArtworkDescriptor[] = [
+  make_atc('2025-09-23', 3),
   {
-    id: '2024-10-02_Card0012',
-    title: 'Card 12/1000',
-    date: '2024-10-12',
+    ...make_atc('2024-10-12', 12),
     timeline_desc: `
         I was reading <cite><a href="https://www.google.com/books/edition/The_Algorithmic_Beauty_of_Plants/4F7lBwAAQBAJ?hl=en&gbpv=0">The Algorithmic Beauty of Plants</a></cite>
         around this time. This inspired the tree-shaped motifs.
     `,
-    sort_key: '2024-10-02:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
     featured: true,
   },
   {
-    id: '2024-10-12_Card0019',
-    title: 'Card 19/1000',
-    date: '2024-10-12',
+    ...make_atc('2024-10-12', 19),
     timeline_desc: `
         This doodle inspired the "nacho" fractal I made in 
         <a href="#/project/math-notebook">Math Notebook</a>. I was trying to
@@ -34,134 +33,19 @@ export const next_1000_cards_artworks: ArtworkDescriptor[] = [
         <a href="https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle">Sierpiński triangle</a>
         fractal, but using curved triangles instead.
     `,
-    sort_key: '2024-10-12:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
   },
-  {
-    id: '2024-10-18_Card0027',
-    title: 'Card 27/1000',
-    date: '2024-10-18',
-    timeline_desc: 'Yet another doodle.',
-    sort_key: '2024-10-18:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2024-11-03_Card0044',
-    title: 'Card 44/1000',
-    date: '2024-11-03',
-    timeline_desc: 'Yet another doodle.',
-    sort_key: '2024-11-03:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2024-12-07_Card0073',
-    title: 'Card NN/1000',
-    date: '2024-12-07',
-    timeline_desc: 'TODO',
-    sort_key: '2024-12-07:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2024-11-15_Card0058',
-    title: 'Card NN/1000',
-    date: '2024-11-15',
-    timeline_desc: 'TODO',
-    sort_key: '2024-11-15:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2024-12-03_Card0071',
-    title: 'Card NN/1000',
-    date: '2024-12-03',
-    timeline_desc: 'TODO',
-    sort_key: '2024-12-03:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2024-11-07_Card0048',
-    title: 'Card NN/1000',
-    date: '2024-11-07',
-    timeline_desc: 'TODO',
-    sort_key: '2024-11-07:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-01-07_Card0085',
-    title: 'Card NN/1000',
-    date: '2025-01-07',
-    timeline_desc: 'TODO',
-    sort_key: '2025-01-07:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-01-26_Card0094',
-    title: 'Card NN/1000',
-    date: '2025-01-26',
-    timeline_desc: 'TODO',
-    sort_key: '2025-01-26:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-02-11_Card0100',
-    title: 'Card NN/1000',
-    date: '2025-02-11',
-    timeline_desc: 'TODO',
-    sort_key: '2025-02-11:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-03-07_Card0112',
-    title: 'Card NN/1000',
-    date: '2025-03-07',
-    timeline_desc: 'TODO',
-    sort_key: '2025-03-07:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-05-06_Card0132',
-    title: 'Card 132/1000',
-    date: '2025-05-06',
-    timeline_desc: 'TODO',
-    sort_key: '2025-05-06:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-05-06_Card150',
-    title: 'Card 150/1000',
-    date: '2025-05-06',
-    timeline_desc: 'TODO',
-    sort_key: '2025-05-06:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-05-25_Card0121',
-    title: 'Card 121/1000',
-    date: '2025-05-25',
-    timeline_desc: 'TODO',
-    sort_key: '2025-05-25:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
-  {
-    id: '2025-06_Card0141',
-    title: 'Card 141/1000',
-    date: '2025-06-??',
-    timeline_desc: 'TODO',
-    sort_key: '2025-06-??:01',
-    project_id: 'next-1000-cards',
-    img_format: 'jpg',
-  },
+  make_atc('2024-10-18', 27),
+  make_atc('2024-11-03', 44),
+  make_atc('2024-12-07', 73),
+  make_atc('2024-11-15', 58),
+  make_atc('2024-12-03', 71),
+  make_atc('2024-11-07', 48),
+  make_atc('2025-01-07', 85),
+  make_atc('2025-01-26', 94),
+  make_atc('2025-02-11', 100),
+  make_atc('2025-03-07', 112),
+  make_atc('2025-05-06', 132),
+  make_atc('2025-05-06', 150),
+  make_atc('2025-05-25', 121),
+  make_atc('2025-06-??', 141),
 ]
