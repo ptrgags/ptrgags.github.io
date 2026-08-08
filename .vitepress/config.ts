530bef863a3d66194d4bbd7edcea2af8d01fe805
Peter Gagliardi <ptrgags@gmail.com>
2026-08-08 19:58:27 -0400

    Add vitepress

---
+++ .vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Peter Gagliardi",
+  description: "My portfolio of creative projects"
+})
