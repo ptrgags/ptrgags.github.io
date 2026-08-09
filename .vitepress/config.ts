26b4795f9d3010effe08d987c4f4b3d18058c2b0
Peter Gagliardi <ptrgags@gmail.com>
2026-08-08 20:08:08 -0400

    add OpenGraph metadata to site config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,27 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Peter Gagliardi",
-  description: "My portfolio of creative projects"
+  srcDir: 'src',
+  title: 'Peter Gagliardi',
+  description: 'My portfolio of creative projects',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { property: 'og:title', content: 'Peter Gagliardi' }],
+    ['meta', { property: 'og:url', content: 'https://ptrgags.dev' }],
+    ['meta', { property: 'og:image', content: '/preview.png' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:description', content: 'My portfolio of creative projects' }],
+  ],
+  /*
+  rewrites: {
+    'path/:slug*': ':slug*'
+  }
+  */
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
 })
