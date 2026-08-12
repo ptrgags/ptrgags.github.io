ceaca67031727a0b988f09b357e6eab4b9408141
Peter Gagliardi <ptrgags@gmail.com>
2026-08-12 06:29:36 -0400

    Set up barebones project page

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -17,6 +17,25 @@ const { site, frontmatter } = useData()
       <li><a href="/api-examples.html">API Examples</a></li>
     </ul>
   </div>
+  <div v-if="frontmatter.layout === 'project'">
+    <div class="vertical">
+      <div class="card-frame">
+        <!-- <CardImage :image="project.card" />-->
+      </div>
+      <div class="plaque half-column">
+        <h1>{{ frontmatter.title }} ({{ frontmatter.years }})</h1>
+        <!-- TODO: Figure out how to compute the link addresses
+        <a class="big-link" v-if="project.github_url" :href="project.github_url">GitHub</a>
+        &nbsp;
+        <a class="big-link" v-if="project.demo_url" :href="project.demo_url">Live Demo</a>
+        -->
+        <Content />
+      </div>
+      <div class="timeline-container">
+        <!-- <ProjectTimeline :entries="timeline_entries" />-->
+      </div>
+    </div>
+  </div>
   <div v-else>
     <Content />
   </div>
