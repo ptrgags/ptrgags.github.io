40e3869bf5b7cd8adfa84fed776fa34acd9c6152
Peter Gagliardi <ptrgags@gmail.com>
2026-09-16 15:56:27 -0400

    Handle aftertouch and pitch wheel

--- src/tool/midi-summary/MIDIFileSummary.vue
+++ src/tool/midi-summary/MIDIFileSummary.vue
@@ -7,6 +7,8 @@ import {
   MIDIMetaTextEvent,
   MIDIMetaType,
   MIDINoteMessage,
+  MIDIPitchWheelMessage,
+  MIDIProgramChangeMessage,
   MIDISysex,
 } from '../../lib/midi/MIDIEvent.ts'
 import type { MIDIFile } from '../../lib/midi/MIDIFile.ts'
@@ -23,9 +25,13 @@ interface MessageSummary {
 class ChannelStats {
   channel: number
   pitch_range = { min: 127, max: 0 }
-  note_count = 0
+
   ccs_used: Set<number> = new Set()
+  note_count = 0
   cc_count = 0
+  pitch_wheel_count = 0
+  aftertouch_count = 0
+  poly_aftertouch_count = 0
 
   summaries: MessageSummary[] = []
 
@@ -41,18 +47,72 @@ class ChannelStats {
   process_message(message: MIDIMessage) {
     if (message instanceof MIDINoteMessage) {
       this.update_pitch_range(message.pitch)
-      this.note_count += 1
+      this.note_count++
     } else if (message instanceof MIDICCMessage) {
       this.ccs_used.add(message.controller)
+      this.cc_count++
+    } else if (message instanceof MIDIProgramChangeMessage) {
+      this.summaries.push({
+        time: '---',
+        type: MIDIMessageType[message.message_type],
+        description: `${message.program_number} (0-indexed)`,
+      })
+    } else if (message instanceof MIDIPitchWheelMessage) {
+      this.pitch_wheel_count++
+    } else if (message.message_type === MIDIMessageType.CHANNEL_AFTERTOUCH) {
+      this.aftertouch_count++
+    } else if (message.message_type === MIDIMessageType.POLY_AFTERTOUCH) {
+      this.poly_aftertouch_count++
     } else {
+      // shouldn't happen
       console.log(MIDIMessageType[message.message_type], message)
-      throw new Error('not implemented')
+      throw new Error('impossible!')
     }
   }
 
-  get summary(): MessageSummary {
-    const description = `Notes: ${this.note_count} [${this.pitch_range.min}, ${this.pitch_range.max}], CCs: ${this.cc_count}, [${this.ccs_used}]`
-    return { time: '---', type: `Channel ${this.channel}`, description }
+  finalize() {
+    if (this.note_count > 0) {
+      const { min, max } = this.pitch_range
+      const pitch_range = `[${min}, ${max}]`
+      this.summaries.push({
+        time: '---',
+        type: 'NOTE_ON/NOTE_OFF',
+        description: `${this.note_count} messages, range: ${pitch_range}`,
+      })
+    }
+
+    if (this.cc_count > 0) {
+      const cc_values = [...this.ccs_used.values()].sort()
+      this.summaries.push({
+        time: '---',
+        type: 'CONTROL_CHANGE',
+        description: `${this.cc_count} messages, CCs: {${cc_values}}`,
+      })
+    }
+
+    if (this.pitch_wheel_count > 0) {
+      this.summaries.push({
+        time: '---',
+        type: 'PITCH_WHEEL_CHANGE',
+        description: `${this.pitch_wheel_count} messages`,
+      })
+    }
+
+    if (this.aftertouch_count > 0) {
+      this.summaries.push({
+        time: '---',
+        type: 'CHANNEL_AFTERTOUCH',
+        description: `${this.aftertouch_count} messages`,
+      })
+    }
+
+    if (this.poly_aftertouch_count > 0) {
+      this.summaries.push({
+        time: '---',
+        type: 'POLY_AFTERTOUCH',
+        description: `${this.poly_aftertouch_count} messages`,
+      })
+    }
   }
 }
 
@@ -104,12 +164,19 @@ function make_summary(file: MIDIFile<RelativeTimingTrack>) {
       stats_by_channel[channel].process_message(event)
     } else {
       console.error(event)
-      throw new Error('not implemented')
+      throw new Error(`impossible!`)
     }
   }
 
   console.log(general_summaries)
-  console.log(stats_by_channel)
+
+  stats_by_channel.forEach((x) => {
+    if (!x) {
+      return
+    }
+    x.finalize()
+    console.log('Channel', x.channel, x.summaries)
+  })
 }
 </script>
 
