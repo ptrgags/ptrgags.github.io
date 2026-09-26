9ab715b132fdeccb36fe8ac65bc04c672876c2d8
Peter Gagliardi <ptrgags@gmail.com>
2026-09-25 20:10:11 -0400

    Mock up making a table

--- src/tool/midi-note-summary/MIDINoteSummary.vue
+++ src/tool/midi-note-summary/MIDINoteSummary.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue'
 import MIDIFilePicker from '../../components/MIDIFilePicker.vue'
-import { DefaultDict } from '../../lib/data_structures/DefaultDict.ts'
-import { MIDINoteMessage } from '../../lib/midi/MIDIEvent.ts'
 import type { MIDIFile } from '../../lib/midi/MIDIFile.ts'
 import type { RelativeTimingTrack } from '../../lib/midi/MIDITrack.ts'
+import { C, E, G, G4 } from '../../lib/music/pitches.ts'
 
 const song_pitches: Ref<number[]> = ref([])
-const pitches_by_measure: Ref<[number, number[]][]> = ref([])
+const pitches_by_measure: Ref<number[][]> = ref([])
 
 function load_file(file: MIDIFile<RelativeTimingTrack>) {
+  /*
   // PR 1: Split MIDIFile<T> into MIDIFile (always relative, few methods)
   // and MIDIAbsTiming (absolute timing, more methods)
   const abs_file = file.absolute_timing()
@@ -34,9 +34,35 @@ function load_file(file: MIDIFile<RelativeTimingTrack>) {
       by_measure.get(bars.toString).add(pitch)
       overall.add(pitch)
     })
+      */
+
+  // Fake the pitches for now - they should be the pitches found in the file
+  // listed from most frequent to least frequent.
+  //
+  // These should really be (pitch, count) pairs, but we'll get there.
+  song_pitches.value = [C, G, E]
+  pitches_by_measure.value = [[C, E], [G]]
 }
 </script>
 
 <template>
   <MIDIFilePicker @load="load_file"></MIDIFilePicker>
+
+  Song pitches: {{ song_pitches }} <br />
+
+  By measure: <br />
+  <table>
+    <thead>
+      <tr>
+        <th>Measure number</th>
+        <th>Pitches</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="[i, row] in pitches_by_measure.entries()" , :key="i">
+        <td>{{ i + 1 }}</td>
+        <td>{{ row }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
