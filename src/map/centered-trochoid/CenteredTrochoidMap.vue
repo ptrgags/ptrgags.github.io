de751acd8cfca989de1e2c26f29c81ea1159ee8f
Peter Gagliardi <ptrgags@gmail.com>
2026-09-26 10:04:15 -0400

    Create a placeholder sketch for the animation setup

--- src/map/centered-trochoid/CenteredTrochoidMap.vue
+++ src/map/centered-trochoid/CenteredTrochoidMap.vue
@@ -2,16 +2,14 @@
 import { ref, type Ref } from 'vue'
 import ParameterMap from './ParameterMap.vue'
 import type { CenteredTrochoidParams } from './CenteredTrochoidParams.ts'
+import CenteredTrochoidAnimation from './CenteredTrochoidAnimation.vue'
 
 const params: Ref<CenteredTrochoidParams> = ref({ radius_small_circle: 0.5, radius_pen: 0.2 })
 </script>
 
 <template>
-  <ParameterMap @param_change="(value) => (params = value)"></ParameterMap>
-  <hr />
-  <div>
-    Selected Parameters: <br />
-    small_circle: {{ params.radius_small_circle }} * R<br />
-    pen: {{ params.radius_pen }} * r <br />
+  <div class="vertical">
+    <ParameterMap @param_change="(value) => (params = value)"></ParameterMap>
+    <CenteredTrochoidAnimation :params="params" />
   </div>
 </template>
